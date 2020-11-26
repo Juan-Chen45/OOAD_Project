459981b819fedbcdffdfb67222e6d890b9bb5824
Aries-Dawn <3055637042@qq.com>
2020-11-27 07:52:35 +0800

    game list的展示部分已经做好了 detail还没有弄 在home page尝试弄一个类似于走马灯的一个用来播报最近较热的游戏的东西。(还没有太弄懂那个返回值)

--- game/models.py
+++ game/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from read_statistic.models import ReadNum_Expand
 from ckeditor_uploader.fields import RichTextUploadingField
+from pyquery import PyQuery as pq
 # Create your models here.
 
 class GameType(models.Model):
@@ -20,6 +21,14 @@ class Game(models.Model,ReadNum_Expand):
     lastupdate_time = models.DateTimeField(auto_now=True)  # 修改时自动改时间
     price = models.FloatField()
     avatar = RichTextUploadingField()
+
+    def get_content_img_url(self):
+        temp = Game.objects.filter(pk=str(self.id)).values('avatar')  # values 获取 Article 数据表中的 content 字段内容
+        html = pq(temp[0]['avatar'])  # pq 方法获取编辑器 html 内容
+        # print(html, "\n", "----")
+        img_path = pq(html)('img').attr('src')  # 截取 html 内容中的路径
+        # print("pic", img_path)
+        return img_path  # 返回第一张图片路径
 
     def __str__(self):
         return self.name
--- read_statistic/utils.py
+++ read_statistic/utils.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 import datetime
 from django.db.models import Sum
 
+
 def cookie_read(request, object):
     # 下面传参的那个model可以是一个类，也可以是一个类的实例.但contenttype实际上建立的是类之间的外键关系
     # ct = ContentType.objects.get_for_model(object)
@@ -39,11 +40,12 @@ def get_7days_data(contenttype):
     date_list = []
     for i in range(7, 0, -1):
         date = today - datetime.timedelta(i)
-        readdetail = ReadDetail.objects.filter(content_type=contenttype,date = date)
-        result = readdetail.aggregate(sum = Sum('read_num'))  #返回一个字典
-        result_list.append(result['sum'] or 0)   #常用的一个判断，如果前面为false就会返回0，因为你有可能没创建过对应日期的ReadDetail，可能会返回none，none在判断中会为false
+        readdetail = ReadDetail.objects.filter(content_type=contenttype, date=date)
+        result = readdetail.aggregate(sum=Sum('read_num'))  # 返回一个字典
+        result_list.append(
+            result['sum'] or 0)  # 常用的一个判断，如果前面为false就会返回0，因为你有可能没创建过对应日期的ReadDetail，可能会返回none，none在判断中会为false
         date_list.append(date.strftime("%m/%d"))
-    return result_list,date_list
+    return result_list, date_list
 
 
 def get_today_hot_data(content_type):
@@ -51,5 +53,3 @@ def get_today_hot_data(content_type):
     readdetail = ReadDetail.objects.filter(content_type=content_type, date=today).order_by("read_num")
     # 只返回前7条
     return readdetail[:7]
-
-
