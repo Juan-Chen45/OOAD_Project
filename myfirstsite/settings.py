f5988dc534e2dc38d74371db1f6f1b7c1afa4d90
fanshun <11811901@mail.sustech.edu.cn>
2020-12-10 18:31:42 +0800

    正在修改login界面

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -59,8 +59,7 @@ ROOT_URLCONF = 'myfirstsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- myfirstsite/views.py
+++ myfirstsite/views.py
@@ -16,7 +16,18 @@ def homepage(request):
     today_hot_data = get_today_hot_data(ct)
 
     # 本地代码这里就不加缓存了，懒得了，后续可以加上
+
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        # 验证过了，说明用户验证也成功了
+        if login_form.is_valid():
+            user = login_form.cleaned_data["user"]
+            login(request, user)
+            return redirect(request.GET.get("from", reverse("home")))
+    else:
+        login_form = LoginForm()
     context = {}
+    context["login_form"] = login_form
     context['date'] = data
     context['days'] = days
     context['today_hot_data'] = today_hot_data
@@ -24,46 +35,6 @@ def homepage(request):
 
 
 def user_login(request):
-    # if request.method == "POST":
-    #     username = request.POST.get("username", "")
-    #     password = request.POST.get("password", "")
-    #     user = authenticate(request, username=username, password=password)
-    #     # 这里获得了request是从哪来的的消息，这样就可以直接redirect回原本页面。取不到就返回首页
-    #     referer = request.META.get("HTTP_REFERER", reverse("home"))
-    #     if user is not None:
-    #         login(request, user)
-    #         # Redirect to a success page.
-    #         return redirect(referer)
-    #     else:
-    #         # Return an 'invalid login' error message.
-    #         return render(request, 'error.html', {"message": "用户名或密码不正确"})
-    # else:
-    #     return render(request,"login.html",{})
-    # ==============================================================================
-    # 这部分是把用户验证放在这里,接下来我们要把用户验证放到forms表单里面
-    # if request.method == "POST":
-    #     login_form = LoginForm(request.POST)
-    #     # 验证过了，数据比较好
-    #     if login_form.is_valid():
-    #         username = login_form.cleaned_data["username"]
-    #         password = login_form.cleaned_data["password"]
-    #         user = authenticate(request, username=username, password=password)
-    #         if user is not None:
-    #             login(request, user)
-    #             # Redirect to a success page.
-    #             # 返回进入login的上一个页面，默认返回home
-    #             return redirect(request.GET.get("from",reverse("home")))
-    #         else:
-    #             # Return an 'invalid login' error message.
-    #             login_form.add_error(None,"用户名或密码不正确")
-    # else:
-    #     login_form = LoginForm()
-    #
-    # context = {}
-    # context["login_form"] = login_form
-    # return render(request,"login.html",context)
-
-    #     ===============================================================================
     if request.method == "POST":
         login_form = LoginForm(request.POST)
         # 验证过了，说明用户验证也成功了
